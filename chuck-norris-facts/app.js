3d276031c3dd81bb919641b7c884bca04c5caf7f
JB Talha Khan <49081433+jbtalhakhan@users.noreply.github.com>
2020-08-26 09:20:10 +0500

    Create app.js

---
+++ chuck-norris-facts/app.js
@@ -0,0 +1,13 @@
+const fact_p = document.getElementById('fact');
+
+function getFact() {
+    axios.get("http://api.icndb.com/jokes/random")
+        .then((response) => {
+            let joke = response.data.value.joke;
+            
+            fact_p.innerHTML = joke;
+        })
+        .catch((err) => {
+            console.log(err); // remove
+        });
+}
